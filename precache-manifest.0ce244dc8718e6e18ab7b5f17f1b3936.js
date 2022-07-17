ec1ed4d87e6d7b290f791deb21cf6fe4508f6273
AjayS24 <ajs2442002@gmail.com>
2022-07-17 20:50:55 +0530

    Updates

---
+++ precache-manifest.0ce244dc8718e6e18ab7b5f17f1b3936.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2486d3b503543df4d37",
+    "url": "/text_editor/static/css/main.c8318563.chunk.css"
+  },
+  {
+    "revision": "c2486d3b503543df4d37",
+    "url": "/text_editor/static/js/main.c2486d3b.chunk.js"
+  },
+  {
+    "revision": "6bf0c419eaec532f37f5",
+    "url": "/text_editor/static/js/1.6bf0c419.chunk.js"
+  },
+  {
+    "revision": "c664886b55569830f520",
+    "url": "/text_editor/static/js/runtime~main.c664886b.js"
+  },
+  {
+    "revision": "4108876c26ada49e978f19e4e755d29a",
+    "url": "/text_editor/static/media/img3rt.4108876c.png"
+  },
+  {
+    "revision": "fb312e7087cfe353246b943b85a8f34c",
+    "url": "/text_editor/index.html"
+  }
+];
